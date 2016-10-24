f4edf0c54f8f0ac951ef99c67cb86585ac985998
Baluta Cristian <cristi.baluta@gmail.com>
2016-10-24 03:20:32 +0300

    Do some iteration on placeholders

--- invoice.ml
+++ invoice.ml
@@ -19,8 +19,9 @@ type commands =
 ;;
 
 let command = ref Help
-let value = ref 0
+let amount = ref 0
 let hours = ref 0
+let email = ref ""
 let series = ref ""
 let nr = ref 0
 let tva = ref 0
@@ -32,7 +33,6 @@ let command_from_string c = match c with
 	| "help" | _ -> Help
 	
 ;;
-let variables = ["::contractor_name::"]
 let read_all_lines file_name =
   let in_channel = open_in file_name in
   let rec read_recursive lines =
@@ -45,52 +45,68 @@ let read_all_lines file_name =
   let _ = close_in_noerr in_channel in
   List.rev (lines)
 ;;
-
-let rec print_numbers oc = function 
-  | [] -> ()
-  | e::tl -> Printf.fprintf oc "%d %d\n" (fst e) (snd e); print_numbers oc tl
+let placeholders = [
+	"::email::";
+	"::contractor_name::";
+	"::rate::";
+	"::amount::";
+	"::tva::";
+	"::amount_total::"
+]
+let rec iterate_placeholders placeholders lineString = match placeholders with
+	| [] -> print_endline "This is the end of the string list!"
+	| placeholder::body -> 
+		begin
+			let new_lineString = Str.global_replace (Str.regexp placeholder) "5667,88" lineString in
+			iterate_placeholders body new_lineString
+		end
 ;;
-let rec print_lines file_o = function 
+(* let rec replace s = function
+  | [] -> ""
+  | e::tl -> Str.global_replace (Str.regexp "::amount::") "5667,88" e
+;; *)
+let make_replacements s = match s with
+	| ss ->
+		iterate_placeholders placeholders ss
+		(* Str.global_replace (Str.regexp "::title::") "title" ss;
+		Str.global_replace (Str.regexp "::amount::") "5667,88" ss *)
+;;
+let rec write_line file_o = function 
   | [] -> ()
   | e::tl -> 
-	  Printf.fprintf file_o "%s\n" (Str.global_replace (Str.regexp "::title::") "title" e);
-	  print_lines file_o tl
+	  Printf.fprintf file_o "%s\n" (make_replacements e);
+	  write_line file_o tl
 ;;
 
 let main =
 begin
 let speclist = [
-	("-value", Arg.Set_int value, "Value of invoice");
+	("-amount", Arg.Set_int amount, "Amount to be paid");
 	("-tva", Arg.Set_int tva, "TVA");
-	("-hours", Arg.Set_int hours, "Hours");
+	("-hours", Arg.Set_int hours, "Amount of worked hours");
+	("-email", Arg.Set_string email, "Email to be replaced in ::email::");
 	("-series", Arg.Set_string series, "Series of invoice");
 	("-nr", Arg.Set_int nr, "Number of invoice");
 	("-rate", Arg.Set_float rate, "Series of invoice");
 	("-exchange-rate", Arg.Set_float exchange_rate, "Series of invoice");
-	(* ("-tva", Arg.Tuple ([Arg.Set_int time_hours ; Arg.Set_int time_minutes]), "Sets creation hours and minutes listed files have to match"); *)
-	(* ("-s", Arg.Symbol (["alpha"; "chrono"; "owner"], sort_files), " Allows to sort listed files alphabetically, chronologically, or by owner"); *)
-	("--", Arg.Rest (fun arg -> print_endline ("The rest contains: " ^ arg)), "Stop interpreting keywords and prints the rest");
 ]
 in let usage_msg = "Usage:"
 in Arg.parse speclist (fun anon -> command := command_from_string anon) usage_msg;
 match !command with
 	| Generate ->
-		print_endline ("Generate new invoice");
-		(* print_endline ("Value: " ^ string_of_int !value);*)
 		let dir = Sys.getcwd() in
 		let children = Sys.readdir dir in
 		let last_invoice_dir = Array.get children (Array.length children -1) in
 		let template = read_all_lines (dir ^ "/0/template.html") in
-		(* List.iter (printf "%s ") template; *)
 	  	let file_o = open_out (last_invoice_dir ^ "/invoice.html") in
-	  	print_lines file_o template;
+	  	write_line file_o template;
 	  	close_out file_o;
-	| List -> 
+	| List ->
 		print_endline ("List existing invoices");
 		let dir = Sys.getcwd() in
 		let children = Sys.readdir dir in
 		Array.iter print_endline children;
-	| Help -> 
+	| Help ->
 		print_endline ("Print help");
 print_endline ("Thank you for using invoice cmd!");
 
